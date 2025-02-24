3facc7c214069602bf779f6fc7063f778b116d86
unknown <350366723@TDSB.ON.CA>
2025-02-24 11:30:13 -0500

    Started the third assignment

---
+++ src/AmbiguousCase.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class AmbiguousCase {
+    public static void main(String[] args) throws Exception {
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Please input an AngleA value:");
+        double angleA = keyboard.nextDouble();
+        System.out.println("Please input value of side a");
+        double a = keyboard.nextDouble();
+        System.out.println("Please input value of side b");
+        double b = keyboard.nextDouble();
+        keyboard.close();
+
+
+
+    }
+}
