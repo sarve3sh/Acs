012eea9603d039778354b23f02ff8f1a0bbd8d9b
Sarveshwaran Rajarman <sarveshwaran.rajaraman@student.tdsb.on.ca>
2025-03-19 11:07:07 -0400

    newtonsmethod

---
+++ src/Newtonsmethod2.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Newtonsmethod2 {
+    public static void main(String[] args) throws Exception {
+        Scanner input = new Scanner(System.in);
+
+        repeat: while (true) {
+            System.out.println("Please enter a decimal/number as a root guess ");
+            double x = input.nextDouble();
+            double newGuess = 0.0;
+
+            System.out.println(x);
+
+            while (true) {
+                double function = 6 * Math.pow(x, 4) - 13 * Math.pow(x, 3) - 18 * x * x + 7 * x + 6;
+                double derivative = 24 * Math.pow(x, 3) - 39 * x * x + 36 * x + 7;
+                newGuess = x - function / derivative; // calculates the next guess
+                if (Math.abs(x - newGuess) > 0.0000000000001) {
+                    System.out.println(newGuess);
+                    x = newGuess;
+                    continue;
+                } else {
+                    System.out.println("The root is " + newGuess);
+                    System.out.println("Do you want to try again?(Y/N)");
+                    if (input.next().charAt(0) == 'n') {
+                        input.close();
+                        break repeat;
+                    } else {
+                        continue repeat; 
+                    }
+                }
+            }
+        }
+    }
+}
