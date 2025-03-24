70f400f768f6976cde32f19c6dbce1f0b9718a22
unknown <sarveshwaran.rajaraman@student.tdsb.on.ca>
2025-03-24 11:38:33 -0400

    oop

--- src/PowerRule.java
+++ src/PowerRule.java
@@ -4,6 +4,14 @@ public class PowerRule {
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
         System.out.println("Please enter coefficients (seperated by space):");
-        keyboard.close();
+        String [] coefficients = keyboard.nextLine().split(" ");
+        System.out.println("Please enter the degrees of the polynomial (seperated by space):");
+        String [] degrees = keyboard.nextLine().split(" ");
+       keyboard.close();
+       //solving for derivative #1
+        for(int i = 0; i<coefficients.length; i++)
+       // assuming the coefficients and degrees are the same length
+        System.out.println("The first derivative is:"+answer1);
+        System.out.println("The second derivative is:"+answer2);
     }
 }
