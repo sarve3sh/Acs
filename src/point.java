2c3cde31af0bc8166b4c0968891a1ecf2f7f69f0
Rajaraman <sarveshwaran.rajaraman@student.tdsb.on.ca>
2025-04-15 10:27:32 -0400

    s

--- src/point.java
+++ src/point.java
@@ -1,4 +1,38 @@
-public class point {
-    private double pointA;
-    private double pointB;
+public class Point {
+    private double x;
+    private double y;
+
+    public Point() {
+    }
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point difference(Point point) {
+
+    }
+
+    public void displayCoordinate() {
+
+    }
+
+    public double distance(Point point) {
+
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+    public void linearEquation(Point point){
+
+    }
+    public Point midPoint(Point point){
+        
+    }
 }
