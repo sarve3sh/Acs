c8d8319b10564ab81037b3e28d17288ec7065788
Sarveshwaran Rajarman <sarveshwaran.rajaraman@student.tdsb.on.ca>
2025-04-23 11:08:06 -0400

    r

---
+++ src/Garage.java
@@ -0,0 +1,26 @@
+public enum Garage { // Garage.java
+
+MERCEDES(1926,145.6) //they are in billions
+
+MCLAREN("Britain","Senna"),
+
+FORD("America","GT"),
+
+TOYOTA("Japan","Supra"),
+
+HYUNDAI("Korea","Tuscon"),
+
+JAGUAR("India","F-type"),
+
+FERRARI("Italy","F40"),
+
+MGMOTOR("China","Hector"),
+
+LADA("Russia","Niva"),
+
+BUGATTI("France","Chirron");
+
+private  double 
+
+
+}
